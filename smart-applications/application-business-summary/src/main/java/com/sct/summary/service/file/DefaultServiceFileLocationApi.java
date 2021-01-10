e207a36aec2247e05509d82dc9d540a2e7f8dad3
xiangxujian <hubeixiang@aliyun.com>
2021-01-10 12:49:36 +0800

    启动的配置项修改

---
+++ smart-applications/application-business-summary/src/main/java/com/sct/summary/service/file/DefaultServiceFileLocationApi.java
@@ -0,0 +1,28 @@
+package com.sct.summary.service.file;
+
+import com.sct.service.core.api.service.file.ServiceFileLocationApi;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultServiceFileLocationApi implements ServiceFileLocationApi {
+
+    @Override
+    public String locationDir() {
+        return Constants.PROGRAM_HOME;
+    }
+
+    @Override
+    public String locationLogDir() {
+        return Constants.PROGRAM_LOG_HOME;
+    }
+
+    @Override
+    public String locationUploadTemporaryDir() {
+        return Constants.PROGRAM_UPLOAD_TemporaryDir;
+    }
+
+    @Override
+    public String locationUploadPermanentDir() {
+        return Constants.PROGRAM_UPLOAD_PermanentDir;
+    }
+}
