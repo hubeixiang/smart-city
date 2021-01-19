dbc128f7d42e4911bebe7643c1417c24131b1c58
xiangxujian <hubeixiang@aliyun.com>
2021-01-19 17:39:17 +0800

    文件定位操作服务接口定义修改

--- smart-services/service-core-api/src/main/java/com/sct/service/core/api/service/file/ServiceFileLocationApi.java
+++ smart-services/service-core-api/src/main/java/com/sct/service/core/api/service/file/ServiceFileLocationApi.java
@@ -1,5 +1,10 @@
 package com.sct.service.core.api.service.file;
 
+import com.sct.commons.file.fileutil.SimpleFileUtil;
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import java.io.File;
+
 /**
  * 不同的服务有不同的实现
  */
@@ -32,4 +37,70 @@ public interface ServiceFileLocationApi {
      * @return
      */
     public String locationUploadPermanentDir();
+
+    /**
+     * 程序自动生成的相关文件保存的目录,可以删除的
+     *
+     * @return
+     */
+    default String locationTemporaryDir() {
+        String absolutePathFileDir = locationLogDir() + File.separator + "runtime";
+        makeSureDirAlreadyExists(absolutePathFileDir);
+        return absolutePathFileDir;
+    }
+
+    /**
+     * 程序自动生成的相关文件,按照日期保存的目录,可以定时删除
+     * 返回目录不包含日期这一级的目录
+     *
+     * @return D:\03Downloads\Browser\daily\
+     */
+    default String locationTemporaryDailyDir() {
+        String absolutePathFileDir = locationLogDir() + File.separator + "runtime_daily";
+        makeSureDirAlreadyExists(absolutePathFileDir);
+        return absolutePathFileDir;
+    }
+
+    /**
+     * 确保 locationTemporaryDailyDir()相对应的目录是存在的
+     * 并返回当前的目录
+     *
+     * @return
+     */
+    default String makeSureLocationTemporaryDailyDir() {
+        String dailyDir = locationTemporaryDailyDir();
+        makeSureDirAlreadyExists(dailyDir);
+        String currentDay = currentDayDir();
+        String absolutePathFileDir = dailyDir + File.separator + currentDay;
+        makeSureDirAlreadyExists(absolutePathFileDir);
+        return absolutePathFileDir;
+    }
+
+    /**
+     * 已经当前日期生成的目录名称
+     *
+     * @return
+     */
+    default String currentDayDir() {
+        return DateFormatUtils.format(System.currentTimeMillis(), "yyyyMMdd");
+    }
+
+    default String datetime2idName() {
+        return DateFormatUtils.format(System.currentTimeMillis(), "yyyyMMddHHmmss");
+    }
+
+    default String timestamp2idName() {
+        return "A" + System.currentTimeMillis();
+    }
+
+    default void makeSureDirAlreadyExists(String absolutePathFileDir) {
+        boolean ok = SimpleFileUtil.createNewDir(absolutePathFileDir, false);
+        if (!ok) {
+            throw new RuntimeException(String.format("create dir=%s failure!", absolutePathFileDir));
+        }
+    }
+
+    default boolean delete(String absolutePathFileDir) {
+        return SimpleFileUtil.delete(absolutePathFileDir);
+    }
 }
