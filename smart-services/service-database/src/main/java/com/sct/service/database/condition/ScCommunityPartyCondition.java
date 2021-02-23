61a52572cc10c7cb463dd2d79c8b0d80c3afeccf
Qi Zhao <fight.xfcyzq@outlook.com>
2021-02-23 22:22:21 +0800

    社区党组织mapper

--- smart-services/service-database/src/main/java/com/sct/service/database/condition/ScCommunityPartyCondition.java
+++ smart-services/service-database/src/main/java/com/sct/service/database/condition/ScCommunityPartyCondition.java
@@ -1,6 +1,17 @@
 package com.sct.service.database.condition;
 
 public class ScCommunityPartyCondition implements ConditionQuery {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public static ScCommunityPartyCondition of() {
         return new ScCommunityPartyCondition();
     }
